6fc70203263e03366ec116645b01a5abd4a1b2b9
Marcin Wisnios <marcinw1998@gmail.com>
2021-11-24 20:14:14 +0100

    Initial commit

---
+++ src/main/java/com/interteam/interpet/api/InterpetApiApplication.java
@@ -0,0 +1,13 @@
+package com.interteam.interpet.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterpetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterpetApiApplication.class, args);
+	}
+
+}
